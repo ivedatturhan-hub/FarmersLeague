ff58f2c7de23894041a2921fceba02c00b3b2275
Ismail Vedat Turhan <i568207@fontysict.nl>
2026-04-10 22:07:12 +0200

    added a seperate constructor for reading the database

--- FarmersLeague.ML/Player.cs
+++ FarmersLeague.ML/Player.cs
@@ -3,7 +3,7 @@
     public class Player
     {
         public int PlayerID { get; private set; }
-        public string Name { get; set; }
+        public string Name { get; private set; }
         public int Age { get; private set; }
         public string Position { get; private set; }
         public int BaseAttack { get; private set; }
@@ -19,7 +19,7 @@
         public int YellowCards { get; private set; } = 0;
         public int RedCards { get; private set; } = 0;
 
-
+         // constructor for adding a new player 
         public Player(string name, int age, string position, int baseAttack, int baseDefence, double marketValue, int composure, int aggression)
         {
             Name = name;
@@ -30,6 +30,28 @@
             MarketValue = marketValue;
             Composure = composure;
             Aggression = aggression;
+        }
+
+        // constructor for loading a player from the database (includes every property)
+        public Player(int playerID, string name, int age, string position, int baseAttack, int baseDefence, double marketValue,
+        bool isAvailable, int condition, int happiness, int composure, int aggression, int seasonGoals, int seasonAssists, int yellowCards, int redCards)
+        {
+            PlayerID = playerID;
+            Name = name;
+            Age = age;
+            Position = position;
+            BaseAttack = baseAttack;
+            BaseDefence = baseDefence;
+            MarketValue = marketValue;
+            IsAvailable = isAvailable;
+            Condition = condition;
+            Happiness = happiness;
+            Composure = composure;
+            Aggression = aggression;
+            SeasonGoals = seasonGoals;
+            SeasonAssists = seasonAssists;
+            YellowCards = yellowCards;
+            RedCards = redCards;
         }
 
     }
