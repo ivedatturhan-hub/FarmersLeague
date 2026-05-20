8b1a74d82ff232027461622b6346166c87fce2fe
Ismail Vedat Turhan <ivedatturhan@gmail.com>
2026-05-20 20:47:12 +0200

    Added 3 new unit testing

---
+++ FarmersLeague.Test/Test1.cs
@@ -0,0 +1,219 @@
+﻿using FarmersLeague.ML;
+using FarmersLeague.ML.DTOs;
+using FarmersLeague.ML.Interfaces;
+using FarmersLeague.ML.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FarmersLeague.Tests
+{
+    [TestClass]
+    public class PlayerManagerTests
+    {
+        [TestMethod]
+        public void UpdatePlayerTeam_If_Budget_Exceeded()
+        {
+            // arrange part
+            // creating a fake team and a player by using the constructor i made in the fake team db below the test method.
+            AdminTeamDTO poorTeam = new AdminTeamDTO { TeamID = 1, Budget = 50 }; // team with only 50 million euros
+            AdminPlayerDTO expensivePlayer = new AdminPlayerDTO { PlayerID = 1, MarketValue = 100 }; // player worth 100 million euros
+
+            ITeamDb fakeTeamDb = new FakeTeamDb(poorTeam);
+            IPlayerDb fakePlayerDb = new FakePlayerDb(expensivePlayer);
+
+
+
+
+
+            // building the manager with the fake dbs
+            PlayerManager manager = new PlayerManager(fakePlayerDb, fakeTeamDb);
+
+            // act and assert part
+
+            Assert.ThrowsException<Exception>(() =>
+            {
+                manager.UpdatePlayerTeam(1, 1);
+            });
+        }
+
+
+        [TestMethod]
+        public void BudgetUpdateTest()
+        {
+            // arrange part
+            // creating a fake team and a player by using the constructors in the fake db
+
+            AdminTeamDTO testingTeam = new AdminTeamDTO { TeamID = 2, Budget = 100 };
+            AdminPlayerDTO testingPlayer = new AdminPlayerDTO {PlayerID = 2, MarketValue = 40 };
+
+
+            ITeamDb testingTeamDb = new FakeTeamDb(testingTeam);
+            IPlayerDb testingPlayerDb = new FakePlayerDb(testingPlayer);
+
+            double expectedUpdatedBudget = 60;
+
+            // building the manager with fake dbs
+
+            TeamManager teamManager = new TeamManager(testingPlayerDb, testingTeamDb);
+            PlayerManager playerManager = new PlayerManager(testingPlayerDb, testingTeamDb);
+
+            //act
+            playerManager.UpdatePlayerTeam(2, 2);
+            double newBudget = testingTeamDb.GetTeamByID(2).Budget;
+
+
+            // assert
+            Assert.AreEqual(expectedUpdatedBudget, newBudget);
+        }
+
+
+
+        [TestMethod]
+        public void TransferPlayer_toThe_sameTeam()
+        {
+            //arrange part
+
+            AdminTeamDTO testingTeam = new AdminTeamDTO { TeamID = 3, Budget = 100 };
+            AdminPlayerDTO testingPlayer = new AdminPlayerDTO { PlayerID = 3, TeamID = 3,MarketValue = 40 };
+
+            ITeamDb testingTeamDb = new FakeTeamDb(testingTeam);
+            IPlayerDb testingPlayerDb = new FakePlayerDb(testingPlayer);
+
+            PlayerManager playerManager = new PlayerManager(testingPlayerDb, testingTeamDb);
+
+
+            // act and assert
+
+            Assert.ThrowsException<Exception>(() =>
+            {
+                playerManager.UpdatePlayerTeam(3, 3);
+            });
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // these fake databases only exist inside the test project!
+    public class FakePlayerDb : IPlayerDb
+    {
+
+        private AdminPlayerDTO fakePlayer;
+
+
+        //constructor to create a fake player
+        public FakePlayerDb(AdminPlayerDTO FakePlayer)
+        {
+            fakePlayer = FakePlayer;
+        }
+
+
+
+
+
+
+        // The manager will call this, so we hand back a player who costs 100 million
+        public AdminPlayerDTO GetPlayerByID(int id)
+        {
+            return fakePlayer;
+        }
+
+
+
+        public void AddPlayer(string Name, int Age, string Position, int BaseAttack, int BaseDefence, double MarketValue, bool IsAvaible, int Condition, int Happiness, int Composure, int Aggression, int SeasonGoals, int SeasonAssists, int YellowCards, int RedCards)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AdminPlayerDTO> GetAllPlayersForAdmin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AdminPlayerDTO> GetPlayersWithNoTeam()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePlayer(AdminPlayerDTO updatedPlayer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AdminPlayerDTO> GetPlayersByTeamID(int teamID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemovePlayerFromTeam(int playerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePlayer(int playerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangePlayerTeam(int playerID, int teamID)
+        {
+            
+        }
+    }
+
+    public class FakeTeamDb : ITeamDb
+    {
+        private AdminTeamDTO fakeTeam;
+
+
+        public FakeTeamDb(AdminTeamDTO FakeTeam)
+        {
+            fakeTeam = FakeTeam;
+        }
+
+        public void CreateTeam(int leagueID, string teamName, double budget, int points, string tactics, bool isUserControlled)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTeam(int teamID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AdminTeamDTO> GetAllTeamsForAdmin()
+        {
+            throw new NotImplementedException();
+        }
+
+        // The manager will call this, so we hand back a team with only 500 euros!
+        public AdminTeamDTO GetTeamByID(int teamID)
+        {
+            return fakeTeam;
+        }
+
+        public void UpdateTeam(AdminTeamDTO updatedTeam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTeamBudget(int teamID, double newBudget) 
+        { 
+            fakeTeam.Budget = newBudget;
+        }
+    }
+}
