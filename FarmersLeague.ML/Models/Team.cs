49ae1eb896c948e208a248c4413cc9d13f0a6ece
İ Vedat Turhan <ivedatturhan@gmail.com>
2026-06-17 16:49:04 +0200

    added methods for calculating a team's attacking and defending power

--- FarmersLeague.ML/Models/Team.cs
+++ FarmersLeague.ML/Models/Team.cs
@@ -22,7 +22,6 @@ namespace FarmersLeague.ML
         public int Points { get; private set; } = 0;
         public TeamTactics Tactics { get; private set; } = TeamTactics.Balanced;
         public bool IsUserControlled { get; private set; }
-        public List<Player> Players { get; private set; } = new List<Player>();
         
 
 
@@ -50,5 +49,49 @@ namespace FarmersLeague.ML
             Tactics = Enum.Parse<TeamTactics>(tacticsString);
             IsUserControlled = isUserControlled;
         }
+
+
+
+        // model methods
+
+
+        // method for calculating the team's attack power in a match
+        public int GetOverallAttack(List<Player> startingPlayers)
+        {
+            int totalAttack = 0;
+            int numberOfPlayers = startingPlayers.Count;
+
+            if (numberOfPlayers < 11) return 0;
+
+            // calculating the avarage attack of the starting players
+            foreach (Player player in startingPlayers)
+            {
+                totalAttack += player.BaseAttack;
+            }
+
+            int teamAttack = totalAttack / numberOfPlayers;
+
+            return teamAttack;
+        }
+
+        // method for calculating the team's defence power in a match
+        public int GetOverallDefence(List<Player> startingPlayers)
+        {
+            int totalDefence = 0;
+            int numberOfPlayers = startingPlayers.Count;
+
+            if (numberOfPlayers < 11) return 0;
+
+            // calculating the avarage attack of the starting players
+            foreach (Player player in startingPlayers)
+            {
+                totalDefence += player.BaseDefence;
+            }
+
+            int teamDefence = totalDefence / numberOfPlayers;
+
+            return teamDefence;
+        }
+
     }
 }
